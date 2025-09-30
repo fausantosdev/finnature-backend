a34ed21ecaa884a095be9e7f693b05116ae61725
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-09-30 10:51:18 -0300

    feat: add AuthGuard for JWT token validation

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,12 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common'
 
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { response } from 'src/common/helpers/response-helper'
+import { AuthGuard } from 'src/guards/auth.guard'
 
 @Controller('/users')
 export class UsersController {
@@ -26,6 +28,7 @@ export class UsersController {
     })
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   async findAll() {
     const result = await this.usersService.findAll()
@@ -35,6 +38,7 @@ export class UsersController {
     })
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const result = await this.usersService.findOne({ id })
@@ -44,6 +48,7 @@ export class UsersController {
     })
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const result = await this.usersService.update(id, updateUserDto)
@@ -53,6 +58,7 @@ export class UsersController {
     })
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     const result = await this.usersService.remove(id)
