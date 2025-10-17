9cf8b7481aed7b7b2e0380ccf130d772b2946e3c
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-17 10:54:59 -0300

    feat: add role and password reset fields to UpdateUserDto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreateUserDto } from './create-user.dto'
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  role?: string
+  password_reset_token?: string
+  password_reset_expires?: string
+}
