4c91bf6f0fca8cda57e354877ee9c03f875cc71a
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-17 13:50:13 -0300

    refactor: add optional fields and adjust types in UpdateUserDto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,8 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types'
+
 import { CreateUserDto } from './create-user.dto'
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   role?: string
   password_reset_token?: string
-  password_reset_expires?: string
+  password_reset_expires?: Date | string
+  created_at?: Date | string
+  updated_at?: Date | string
 }
