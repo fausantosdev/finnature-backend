1be0ddffbf28bc40654df187bfb9c560eade4793
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-17 13:32:20 -0300

    refactor: adjust user model types to accept both Date and string values

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,12 +1,12 @@
 export class User {
   id: string
   name: string
-  date_of_birth: Date
+  date_of_birth: Date | string
   email: string
   password_hash: string
   role: string
   password_reset_token: string
-  password_reset_expires: string
-  created_at: Date
-  updated_at: Date
+  password_reset_expires: Date | string
+  created_at: Date | string
+  updated_at: Date | string
 }
