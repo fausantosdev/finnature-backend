73221a042d2b015da1c15ea2cec23b8d28e6be23
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-10-17 14:03:04 -0300

    fix: fix findOne method typing to accept optional parameters

--- src/modules/users/repository/user.repository.ts
+++ src/modules/users/repository/user.repository.ts
@@ -5,7 +5,7 @@ import { User } from '../entities/user.entity'
 export abstract class UserRepository {
   abstract create(data: CreateUserDto): Promise<User>
   abstract read(where: object): Promise<User[]>
-  abstract findOne(where: { id: string; email: string }): Promise<User | null>
+  abstract findOne(where: { id?: string; email?: string }): Promise<User | null>
   abstract update(where: object, data: UpdateUserDto): Promise<User>
   abstract delete(where: object): Promise<object | null>
 }
